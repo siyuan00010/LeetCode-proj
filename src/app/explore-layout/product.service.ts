e7b3896b58f165cda56e2a4803b67cf64ffdc1cf
Siyuan <sy00010@mix.wvu.edu>
2022-03-18 20:52:55 -0400

    add firebase

--- src/app/explore-layout/product.service.ts
+++ src/app/explore-layout/product.service.ts
@@ -1,5 +1,6 @@
 // Creat a service component to store backend info
-import { HttpClient } from "@angular/common/http";
+// import { HttpClient } from "@angular/common/http";
+import { AngularFireModule } from "@angular/fire/";
 import { exploreItemModel } from "./explore-item.model";
 import { Injectable } from "@angular/core";
 
@@ -9,15 +10,17 @@ import { Injectable } from "@angular/core";
 export class ProductsService {
   private baseUrl:string= "https://leetcode-590a6-default-rtdb.firebaseio.com/.jason";
   private productsEndPoint:string="exploreCategory.jason";
-  constructor(private http:HttpClient){
+  constructor(private db:){
     
   }
 //get product info from backend
   public getProducts(){
-    return this.http.get<exploreItemModel []>(this.baseUrl + this.productsEndPoint);
+    //return this.http.get<exploreItemModel []>(this.baseUrl + this.productsEndPoint);
+  
   }
    public getProduct(index:number){
-    return this.http.get<exploreItemModel []>(this.baseUrl + 'list/' + index + '.json');
+    //return this.http.get<exploreItemModel []>(this.baseUrl + 'list/' + index + '.json');
+  
   }
       
 }
