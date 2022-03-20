aeb31be8c98ee4891cbb19565b6ad2926902f1cc
Siyuan <sy00010@mix.wvu.edu>
2022-03-20 16:17:12 -0400

    setup environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB4DnQ2OcN1TVVl5S0i-AhgyHCJIc-MNWE",
+  authDomain: "leetcode-590a6.firebaseapp.com",
+  databaseURL: "https://leetcode-590a6-default-rtdb.firebaseio.com",
+  projectId: "leetcode-590a6",
+  storageBucket: "leetcode-590a6.appspot.com",
+  messagingSenderId: "980756200166",
+  appId: "1:980756200166:web:f958ebb5a260304756d927",
+  measurementId: "G-K40VVN0PDF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const environment = {
   production: false,
   firebase: {
@@ -12,3 +33,4 @@ export const environment = {
     measurementId: 'G-K40VVN0PDF',
   }
 };
+
