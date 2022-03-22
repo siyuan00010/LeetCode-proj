9122ec9f50ee0360ec2833ec776782520eebf4b9
Siyuan <sy00010@mix.wvu.edu>
2022-03-21 23:02:37 -0400

    read data from database

--- src/app/explore-layout/products.service.ts
+++ src/app/explore-layout/products.service.ts
@@ -3,28 +3,28 @@
 import { exploreCate } from "./explore-item.model";
 import { Injectable} from "@angular/core";
 import { AngularFireDatabase } from "@angular/fire/compat/database";
-//import { Observable } from "rxjs";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService{
-  //items: Observable<Object[]>
+  items: Observable<Object[]>
   constructor(private db: AngularFireDatabase) {
     // console.log("Setting up Fire Communication")
-    // this.items = this.db.list<exploreCate>('exploreCate').valueChanges();
+    this.items = this.db.list<exploreCate>('exploreCate').valueChanges();
   }
   public getProducts() {
     return this.db.list<exploreCate>("exploreCate").valueChanges();
   }
-  // public showData(){
-  //   this.items.subscribe((data) => {
-  //     console.log("Data Recieved");
-  //     for (let item of data){
-  //       console.log(item);
-  //     }
-  //   })
-  // }
+  public showData(){
+    this.items.subscribe((data) => {
+      console.log("Data Recieved");
+      for (let item of data){
+        console.log(item);
+      }
+    })
+  }
 
 }
 /*use httpClient to read data from backend*/ 
